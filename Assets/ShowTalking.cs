4de8b89ddf2fe74dc6952539f42bde6b5d257c5c
Pat <patrickjohnpaderes8@gmail.com>
2025-06-25 05:45:49 +0800

    revised congratulatory

--- Assets/ShowTalking.cs
+++ Assets/ShowTalking.cs
@@ -6,7 +6,7 @@ public class ShowTalking : MonoBehaviour
 {
     public GameObject walking, talking;
     public AudioSource talk;
-    public GameObject Menu;
+    public GameObject Menu, congrats;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,6 +14,7 @@ public class ShowTalking : MonoBehaviour
         {
             walking.SetActive(false);
             talking.SetActive(true);
+            congrats.SetActive(true);
             talk.Play();
 
             // Start coroutine to wait until audio finishes
@@ -26,6 +27,7 @@ public class ShowTalking : MonoBehaviour
         // Wait while the audio is playing
         yield return new WaitWhile(() => talk.isPlaying);
         Menu.SetActive(true);
+        congrats.SetActive(false);
         // Audio finished
         Debug.Log("done");
     }
